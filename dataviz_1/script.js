1f58215359736c34821bde448c3b9f2abfb3278f
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2023-03-15 13:50:49 -0300

    fix: removed unused code

--- dataviz_1/script.js
+++ dataviz_1/script.js
@@ -147,39 +147,6 @@ async function main(){
         await navigate(country, p1, p2, ip, iv);
     }
 }
-// function render(country, arc) {
-//     context.clearRect(0, 0, width, height);
-//     context.beginPath(), path(land), context.fillStyle = "#ccc", context.fill();
-//     context.beginPath(), path(country), context.fillStyle = "#f00", context.fill();
-//     context.beginPath(), path(borders), context.strokeStyle = "#fff", context.lineWidth = 0.5, context.stroke();
-//     context.beginPath(), path(sphere), context.strokeStyle = "#000", context.lineWidth = 1.5, context.stroke();
-//     context.beginPath(), path(arc), context.stroke();
-//     return context.canvas;
-// }
-
-// let p1, p2 = [0, 0], r1, r2 = [0, 0, 0];
-// for (const country of countries) {
-//     let name = country.properties.name;
-//     yield render(country);
-
-//     p1 = p2, p2 = d3.geoCentroid(country);
-//     r1 = r2, r2 = [-p2[0], tilt - p2[1], 0];
-//     const ip = d3.geoInterpolate(p1, p2);
-//     const iv = Versor.interpolateAngles(r1, r2);
-
-//     await d3.transition()
-//         .duration(1250)
-//         .tween("render", () => t => {
-//             projection.rotate(iv(t));
-//             render(country, { type: "LineString", coordinates: [p1, ip(t)] });
-//         })
-//         .transition()
-//         .tween("render", () => t => {
-//             render(country, { type: "LineString", coordinates: [ip(t), p2] });
-//         })
-//         .end();
-// }
-
 class Versor {
     static fromAngles([l, p, g]) {
         l *= Math.PI / 360;
