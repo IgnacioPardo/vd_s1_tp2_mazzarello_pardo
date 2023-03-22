2100349ea0521f0280dbc43decf61e4a3a8edf6a
Luquish <lucamazza02@gmail.com>
2023-03-21 22:43:29 -0300

    Histograma piolon

--- dataviz_4/script.js
+++ dataviz_4/script.js
@@ -2,7 +2,6 @@ var margin = {top: 10, right: 30, bottom: 30, left: 40},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
 var svg = d3.select("#chart")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -10,73 +9,65 @@ var svg = d3.select("#chart")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// get the data
-d3.csv("./vd_astronautas/astronautas.csv", function(data) {
 
-  // X axis: scale and draw:
+d3.csv("/Users/lucamazzarello_/Desktop/Repos/Visualizacion_de_datos/vd_s1_tp2_mazzarello_pardo/vd_astronautas/astronautas.csv", function(data) {
+
+
   var x = d3.scaleLinear()
-      .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-      .range([0, width]);
+      .range([0, width])
+      .domain([0, d3.max(data, function(d) { return +d.anio_mision; })])
   svg.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
-  // Y axis: initialization
-  var y = d3.scaleLinear().range([height, 0]);
-  var yAxis = svg.append("g")
 
-  // A function that builds the graph for a specific value of bin
+  var y = d3.scaleLinear()
+      .range([height, 0])
+      .domain([0, d3.max(data, function(d) { return +d.cantidad_misiones; })]);
+  var yAxis = svg.append("g")
+      .call(d3.axisLeft(y));
+
+
   function update(nAnio_mision) {
 
-    // set the parameters for the histogram
     var histogram = d3.histogram()
-        .value(function(d) { return d.nombre; })   // I need to give the vector of value
-        .domain(x.domain())  // then the domain of the graphic
-        .thresholds(x.ticks(nAnio_mision)); // then the numbers of bins
-
-    // And apply this function to data to get the bins
+        .value(function(d) { return +d.anio_mision; })   
+        .domain(x.domain()) 
+        .thresholds(x.ticks(nAnio_mision)); 
+  
+    
     var anio_mision = histogram(data);
-
-    // Y axis: update now that we know the domain
-    y.domain([0, d3.max(anio_mision, function(d) { return d.mision_hs; })]);   // d3.hist has to be called before the Y axis obviously
+  
+    
+    y.domain([0, d3.max(anio_mision, function(d) { return d.length; })]);   
     yAxis
         .transition()
         .duration(1000)
         .call(d3.axisLeft(y));
-
-    // Join the rect with the anio_mision data
-    var u = svg.selectAll("rect")
-        .data(anio_mision)
-
-    // Manage the existing bars and eventually the new ones:
-    u
-        .enter()
-        .append("rect") // Add a new rect for each new elements
-        .merge(u) // get the already existing elements as well
-        .transition() // and apply changes to all of them
+  
+    
+    var bars = svg.selectAll("rect")
+        .data(bins);
+  
+    
+    bars.enter()
+        .append("rect")
+        .merge(bars)
+        .transition()
         .duration(1000)
-          .attr("x", 1)
-          .attr("transform", function(d) { return "translate(" + x(d.nombre) + "," + y(d.mision_hs) + ")"; })
-          .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-          .attr("height", function(d) { return height - y(d.mision_hs); })
-          .style("fill", "#69b3a2")
-
-
-    // If less bar in the new histogram, I delete the ones not in use anymore
-    u
-        .exit()
-        .remove()
-
-    }
-
-
-  // Initialize with 20 anio_mision
-  update(2010)
-
-
-  // Listen to the button -> update if user change it
-  d3.select("#nAnio_mision").on("input", function() {
-    update(+this.value);
-  });
-
+        .attr("x", 1)
+        .attr("transform", function(d) {
+            return "translate(" + x(d.x0) + "," + y(d.length) + ")";
+        })
+        .attr("width", function(d) {
+            return x(d.x1) - x(d.x0) - 1;
+        })
+        .attr("height", function(d) {
+            return height - y(d.length);
+        });
+  
+    bars.exit().remove();
+  }
+  
+  update(10);
 });
